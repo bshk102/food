628ca27805a090abd3fd5db81bda4603a60ea676
Your God <ass@hole>
2021-02-02 13:26:32 +0500

    make timer

--- js/script.js
+++ js/script.js
@@ -28,20 +28,42 @@ window.addEventListener('DOMContentLoaded', () => {
     showTabContent();
 
     //Timer
-    const deadline = new Date(2021, 2, 1),
-        dlDays = deadline.getDate(),
-        dlHours = deadline.getHours(),
-        dlMinutes = deadline.getMinutes(),
-        dlSeconds = deadline.getSeconds(),
+    const deadline = new Date(2021, 1, 2, 13, 26),
         htmlDays = document.querySelector('#days'),
         htmlHours = document.querySelector('#hours'),
         htmlMinutes = document.querySelector('#minutes'),
         htmlSeconds = document.querySelector('#seconds');
-        
-    const timer = setInterval(() => {
+
+    const getTimeLeft = (deadline) => {
         let currentDate = new Date();
         let timeLeft = deadline - currentDate;
 
+        let days = Math.floor(timeLeft/1000/60/60/24),
+            hours = Math.floor((timeLeft/1000/60/60)%24),
+            minutes = Math.floor((timeLeft/1000/60)%60),
+            seconds = Math.floor((timeLeft/1000)%60);
         
+            return { timeLeft, days, hours, minutes, seconds };
+    };
+
+    const renderTimeLeft = (timerHTML, timeLeft) => {
+        if (timeLeft.timeLeft <= 0) {
+            timerHTML.querySelector('.title').textContent = 'Акция завершена';
+            timerHTML.querySelector('.timer').innerHTML = '';
+            clearInterval(startTimer);
+        }
+        htmlDays.textContent = drawZero(timeLeft.days);
+        htmlHours.textContent = drawZero(timeLeft.hours);
+        htmlMinutes.textContent = drawZero(timeLeft.minutes);
+        htmlSeconds.textContent = drawZero(timeLeft.seconds);
+        console.log('clock');
+    };
+
+    const drawZero = num => num < 10 ? `0${num}` : num;
+
+    const startTimer = setInterval(() => {
+        renderTimeLeft(document.querySelector('.promotion__timer'), getTimeLeft(deadline));
     }, 1000);
+    
+    renderTimeLeft(document.querySelector('.promotion__timer'), getTimeLeft(deadline));
 });
