34f831dd459a5ea1ac624f87b665d7b859a50134
Your God <ass@hole>
2021-02-02 14:44:38 +0500

    make smooth scroll

--- js/script.js
+++ js/script.js
@@ -66,4 +66,29 @@ window.addEventListener('DOMContentLoaded', () => {
     }, 1000);
     
     renderTimeLeft(document.querySelector('.promotion__timer'), getTimeLeft(deadline));
+
+    //скролл
+    const headerLinks = document.querySelectorAll('.header__links a'),
+        menu = document.querySelector('.menu'),
+        order = document.querySelector('.order');
+
+    const smoothScroll = (destination, speed) => {
+        let height = 0;
+        setInterval(() => {
+            if (height <= destination.offsetTop) {
+                height += 10;
+                window.scrollTo(0, height);
+            }
+        }, speed);
+    };
+    
+    headerLinks[0].addEventListener('click', (e) => {
+        e.preventDefault();
+        smoothScroll(order, 10);
+    });
+
+    headerLinks[1].addEventListener('click', (e) => {
+        e.preventDefault();
+        smoothScroll(menu, 10);
+    });
 });
